4e74644bcf47b9ba2cda210b3a61b060486e3f4a
Dani <danielg2964@gmail.com>
2026-02-13 11:24:40 -0500

    Change: Move from Roslyn to EasyDotnet

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,15 +1,28 @@
-vim.pack.add{
-  {
+vim.pack.add{{
     src = 'https://github.com/neovim/nvim-lspconfig',
     name = 'nvim-lspconfig' 
-  },
-}
+}}
+
+vim.pack.add{{
+    src = 'https://github.com/GustavEikaas/easy-dotnet.nvim',
+    name = 'easy-dotnet' 
+}}
+
+vim.pack.add{{
+    src = 'https://github.com/nvim-lua/plenary.nvim',
+    name = 'plenary.nvim' 
+}}
 
 -- vtsls
 vim.lsp.enable('vtsls')
 
--- roslyn
-vim.lsp.enable('roslyn_ls')
+require('easy-dotnet').setup({
+  auto_bootstrap_namespace = {
+    --block_scoped, file_scoped
+    type = "block_scoped",
+    enabled = true,
+  },
+})
 
 -- gopls
 vim.lsp.enable('gopls')
